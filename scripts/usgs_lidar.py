ac23ee04306fe2b6bc34fe0fb3e1ccd133681b90
sel6 <kabodshekinah@gmail.com>
2022-06-24 21:52:47 +0300

    add logger to script

--- scripts/usgs_lidar.py
+++ scripts/usgs_lidar.py
@@ -8,10 +8,14 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon, Point, mapping
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 class UsgsLidar:
     
     def __init__(self, path = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/", pipeline_json_path: str="../pipeline.json") -> None:
+        
+        logging.basicConfig(filename="../logs/keep_track.log", level=logging.INFO, format="time: %(asctime)s, function: %(funcName)s, module: %(name)s, message: %(message)s \n")
             
         self.path = path
         self.txt = self.read_txt("../data/filenames.txt")
@@ -22,10 +26,11 @@ class UsgsLidar:
         try:
             with open(json_path) as js:
                 json_obj = json.load(js)
+                logging.info("read json file successfully!")
             return json_obj
 
         except FileNotFoundError:
-            print('File not found.')
+            logging.exception('File not found.')
         
     def fetch_polygon_boundaries(self, polygon: Polygon):
         polygon_df = gpd.GeoDataFrame([polygon], columns=['geometry'])
@@ -40,16 +45,23 @@ class UsgsLidar:
             polygon_input += f'{x} {y}, '
         polygon_input = polygon_input[:-2]
         polygon_input += '))'
-
+        
+        logging.info("successfully fetched polygon boundaries")
+        
         return f"({[minx, maxx]},{[miny,maxy]})", polygon_input
     
     def read_csv(self, csv_path, missing_values=["n/a", "na", "undefined"]):
         try:
             df = pd.read_csv(csv_path, na_values=missing_values)
+            logging.info("read csv successfully!")
+                         
             return df
 
         except FileNotFoundError:
+                         
+            logging.exception("failed to load file!")             
             print('File not found.')
+            
             
     def fetch_pipeline (self, region: str, polygon: Polygon):
         url = f"{self.path}{region}/ept.json"
@@ -60,6 +72,7 @@ class UsgsLidar:
         self.a['pipeline'][0]['bounds'] = boundary
         pipeline = pdal.Pipeline(json.dumps(self.a))
         
+        logging.info("loaded pipe successfully!")
         return pipeline
     
     def execute_pipeline(self, polygon: Polygon, epsg=4326, region: str = "IA_FullState"):
@@ -68,9 +81,11 @@ class UsgsLidar:
 
         try:
             pipeline.execute()
+            logging.info("pipeline executed successfully!")
+                              
             return pipeline
         except RuntimeError as e:
-            print('Pipeline execution failed')
+            logging.exception('Pipeline execution failed')
             print(e)
     
     
@@ -90,22 +105,31 @@ class UsgsLidar:
             gpd_df['geometry'] = geometry
             gpd_df = gpd_df.set_geometry("geometry")
             gpd_df.set_crs(epsg = epsg, inplace=True)
+                              
+            logging.info("created geopandas dataframe successfully!")
+                              
             return gpd_df
         except RuntimeError as e:
+            logging.exception("failed to create geopandas")
             print(e)
 
     def fetch_region_data(self, polygon: Polygon, epsg=4326):
         pipeline = self.execute_pipeline(polygon, epsg)
+        
+        logging.info("fetched region data successfully!")
+                              
         return self.create_gpd_df(epsg, pipeline)
     
     def read_txt(self, txt_path) -> list:
         try:
             with open(txt_path, "r") as f:
                 text_file = f.read().splitlines()
-            
+                logging.info("read text file successfully!")
+                              
             return text_file
 
         except Exception as e:
+            logging.exception("failed to load text file")
             print(e)
             
     def fetch_name_and_year(self, location: str) -> tuple:
@@ -114,8 +138,10 @@ class UsgsLidar:
         regex = '20[0-9][0-9]+'
         match = re.search(regex, location)
         if(match):
+          logging.info("there is a name and year match!")
           return (location[:match.start() - 1], location[match.start():match.end()])
         else:
+          logging.info("there is no match!")
           return (location, None)
     
    
@@ -143,6 +169,7 @@ class UsgsLidar:
 
             metadata.to_csv("../data/metadata.csv")
         
+        logging.info("returned metadata successfully!")
         return(metadata)
     
     
@@ -166,6 +193,7 @@ class UsgsLidar:
         if(len(df)==0):
             print("polygon is not located")
         
+        logging.info("fetched region successfully!")
         return regions   
     
     def fetch_data(self, polygon: Polygon, region="IA_FullState") -> dict:
@@ -184,7 +212,8 @@ class UsgsLidar:
             
                 if region_df.empty == False:
                     region_dicto[year] = region_df
-
+        
+        logging.info("fetched data successfully")
         return(region_dicto)
     
     def plot_terrain(self, gdf: gpd.GeoDataFrame, fig_size: tuple=(12, 10), size: float=0.01):
@@ -193,7 +222,9 @@ class UsgsLidar:
         ax = plt.axes(projection='3d')
         ax.scatter(gdf.geometry.x, gdf.geometry.y, gdf.elevation, s=size)
         plt.show()
-    
+        
+        logging.info("plotted terrain successfully!")
+                              
     def save_heatmap(self, df, png_path, title) -> None:
         fig, ax = plt.subplots(1, 1, figsize=(12, 10))
         df.plot(column='elevation', ax=ax, legend=True, cmap="terrain")
@@ -204,13 +235,17 @@ class UsgsLidar:
         plt.axis('off')
         plt.close()
         
+        logging.info("saved hitmap successfully!")
+        
     def load_heatmap(self, png_path):
         fig, ax = plt.subplots(1, 1, figsize=(12, 10))
         img = mp.imread(png_path)
         imgplot = plt.imshow(img)
         plt.axis('off')
         plt.show() 
-    
+        
+        logging.info("loaded hitmap successfully!")
+                              
 if __name__=="__main__":
     US = UsgsLidar()
     MINX, MINY, MAXX, MAXY = [-93.759055, 41.925015, -93.766155, 41.935015]
