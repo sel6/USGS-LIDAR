43ae5bc32410ac146c6e59c85f700d6db3ac14ec
Selam <84750917+sel6@users.noreply.github.com>
2022-06-25 21:45:21 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,22 @@
 
+import os
+import sys
+import unittest
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join('../scripts')))
+from usgs_lidar import UsgsLidar as US
+
+class TestFiles(unittest.TestCase):
+  
+  def setUp(self):
+    self.helper = US()
+
+  def test_read_csv(self):
+    df = US..read_csv('test.csv')
+    df1 = pd.read_csv('test.csv')
+    self.assertEqual(df.shape, df1.shape)
+    
+    
+if __name__ == '__main__':
+  unittest.main()
